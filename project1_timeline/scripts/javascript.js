e21b9a908eb6ec1cfdc62d61896acb212da66e5e
kaiuswu <kaiuswu@users.noreply.github.com>
2019-03-08 17:57:46 -0500

    hell yeah the snake wiggles

--- project1_timeline/scripts/javascript.js
+++ project1_timeline/scripts/javascript.js
@@ -1,10 +1,19 @@
-var audio = $("#mySoundClip")[0];
-$("nav a").mouseenter(function() {
-  audio.play();
+
+//
+// function PlaySound(soundobj) {
+//     var thissound=document.getElementById(soundobj);
+//     thissound.play();
+// }
+
+
+
+$(".swap-on-hover").hover(function() {
+  console.log("hovering");
+  // $(".swap-on-hover__front-image").css("display", "none");
+  // $(".swap-on-hover__back-image").css("display", "inline");
+  $(".swap-on-hover__front-image").toggle();
+  $(".swap-on-hover__back-image").toggle();
+
 });
 
-
-function PlaySound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.play();
-}
+console.log("hello");
