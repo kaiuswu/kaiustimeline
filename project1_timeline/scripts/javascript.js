c5383df80754a3cae3b9fc72aed084cba93de061
kaiuswu <kaiuswu@users.noreply.github.com>
2019-03-24 18:33:35 -0400

    redacted finalization + spinning

--- project1_timeline/scripts/javascript.js
+++ project1_timeline/scripts/javascript.js
@@ -1,19 +1,10 @@
 
-//
-// function PlaySound(soundobj) {
-//     var thissound=document.getElementById(soundobj);
-//     thissound.play();
-// }
-
-
-
 $(".swap-on-hover").hover(function() {
   console.log("hovering");
   // $(".swap-on-hover__front-image").css("display", "none");
   // $(".swap-on-hover__back-image").css("display", "inline");
   $(".swap-on-hover__front-image").toggle();
   $(".swap-on-hover__back-image").toggle();
-  $("")
 });
 
 $( function() {
